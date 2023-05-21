8d36ef5f679b1a75d6d461ffd750e0ea665e0bea
daniel-lee-user <danielhlee02@gmail.com>
2023-05-21 15:30:16 -0700

    Added node modules,, tests, etc

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-        const name = 'James'
-
-const person = {first: name}
-
-console.log(person)
-
-const sayHelloLinting = (fName) => {
-console.log(`Hello linting, ${fName}`);
-};
-
-sayHelloLinting();
+test('1+1=2', () => {
+    expect(1+1).toBe(2);
+});
