d78f69237875009f2693412dd0a9979d9e7bc550
Elvis Joa <ejoa@ucsd.edu>
2023-05-21 23:20:52 -0700

    Made a basic home page layout with functioning buttons closes #5

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,7 @@
-/* eslint-disable linebreak-style */
-const FORTUNETYPES = {
-    tarotCard: "tarot",
-    egg: "egg",
-    bone: "bone"
-};
-const FORTUNELIST = ["tarot", "egg", "bone"];
+import * as consts from "./consts";
+
 /**
- * Binds the home page buttons to change the type of fortune telling that is displayed
+ * Binds the home page buttons to change the type of consts.FORTUNE telling that is displayed
  * @author Elvis Joa
  * @date 5/21/2023
  */
@@ -15,26 +10,26 @@ function bind_home_page_btns(){
     const eggBtn = document.getElementById("toEgg");
     const boneBtn = document.getElementById("toBoneTossing");
     tarotCardBtn.addEventListener("click", () => {
-        console.log(FORTUNETYPES.tarotCard);
-        display_general_UI_elements(FORTUNETYPES.tarotCard);
-        document.getElementById("center-text").textContent = FORTUNETYPES.tarotCard;
+        console.log(consts.FORTUNETYPES.tarotCard);
+        display_general_UI_elements(consts.FORTUNETYPES.tarotCard);
+        document.getElementById("center-text").textContent = consts.FORTUNETYPES.tarotCard;
     });
 
     eggBtn.addEventListener("click", () => {
-        console.log(FORTUNETYPES.egg);
-        display_general_UI_elements(FORTUNETYPES.egg);
-        document.getElementById("center-text").textContent = FORTUNETYPES.egg;
+        console.log(consts.FORTUNETYPES.egg);
+        display_general_UI_elements(consts.FORTUNETYPES.egg);
+        document.getElementById("center-text").textContent = consts.FORTUNETYPES.egg;
     });
 
     boneBtn.addEventListener("click", () => {
-        console.log(FORTUNETYPES.bone);
-        display_general_UI_elements(FORTUNETYPES.bone);
-        document.getElementById("center-text").textContent = FORTUNETYPES.bone;
+        console.log(consts.FORTUNETYPES.bone);
+        display_general_UI_elements(consts.FORTUNETYPES.bone);
+        document.getElementById("center-text").textContent = consts.FORTUNETYPES.bone;
     });
 }
 
 /**
- * Binds the buttons that appear at the top left for all fortune telling,
+ * Binds the buttons that appear at the top left for all consts.FORTUNE telling,
  * more specifically the home, options, and GitHub buttons.
  * @author Elvis Joa
  * @date 5/21/2023
@@ -52,7 +47,7 @@ function bind_general_buttons(){
  * @author Elvis Joa
  * @date 5/21/2023
  */
-function display_general_UI_elements(fortuneType=null) {
+function display_general_UI_elements(fortuneType =null) {
     // Changes the general buttons (home, options, GitHub)
     const generalBtns = document.getElementsByClassName("general");
     for (let i = 0; i < generalBtns.length; i++) {
@@ -61,10 +56,10 @@ function display_general_UI_elements(fortuneType=null) {
 
     //Hides/Displays the option button that is needed if any
     for (let i = 0; i <= 2; i++) {
-        const optionsBtn = document.getElementById(FORTUNELIST[i]+"Options");
+        const optionsBtn = document.getElementById(consts.FORTUNELIST[i]+"Options");
         optionsBtn.hidden = true;
     }
-    if (fortuneType != null) {
+    if (FORTUNEType != null) {
         const optionsBtn = document.getElementById(fortuneType + "Options");
         optionsBtn.hidden = false;
     }
