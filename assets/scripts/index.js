4b90a5d782e23aa833d87d1c1bf5e5a9ed758426
Elvis Joa <ejoa@ucsd.edu>
2023-05-23 23:02:25 -0700

    Added text display at the bottom of the screen

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,4 +1,4 @@
-import * as consts from "./consts";
+import * as consts from "./consts.js";
 /**
  * Binds the home page buttons to change the type of consts.FORTUNE telling that is displayed
  * @author Elvis Joa
@@ -12,18 +12,21 @@ function bind_home_page_btns(){
         console.log(consts.FORTUNETYPES.tarotCard);
         display_general_UI_elements(consts.FORTUNETYPES.tarotCard);
         document.getElementById("center-text").textContent = consts.FORTUNETYPES.tarotCard;
+        document.getElementsByClassName("response")[0].textContent = "THIS IS THE RESPONSE FOR THE TAROT CARD";
     });
 
     eggBtn.addEventListener("click", () => {
         console.log(consts.FORTUNETYPES.egg);
         display_general_UI_elements(consts.FORTUNETYPES.egg);
         document.getElementById("center-text").textContent = consts.FORTUNETYPES.egg;
+        document.getElementsByClassName("response")[0].textContent = "THIS IS THE RESPONSE FOR THE EGG";
     });
 
     boneBtn.addEventListener("click", () => {
         console.log(consts.FORTUNETYPES.bone);
         display_general_UI_elements(consts.FORTUNETYPES.bone);
         document.getElementById("center-text").textContent = consts.FORTUNETYPES.bone;
+        document.getElementsByClassName("response")[0].textContent = "THIS IS THE RESPONSE FOR THE BONE TOSSING";
     });
 }
 
@@ -38,6 +41,7 @@ function bind_general_buttons(){
     homeBtn.addEventListener("click", () => {
         display_general_UI_elements();
         document.getElementById("center-text").textContent = "";
+        document.getElementsByClassName("response")[0].textContent = "";
     });
 }
 
