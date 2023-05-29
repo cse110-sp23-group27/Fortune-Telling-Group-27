e11bed5921cc244a867e52a2015fc5fee9978628
GitHub Action <action@github.com>
2023-05-29 02:57:36 +0000

    Added Pipeline Changes Automatically

--- assets/scripts/TaroCard.js
+++ assets/scripts/TaroCard.js
@@ -1,8 +1,8 @@
-class TarotCard{
-    constructor(){
+class TarotCard {
+	constructor() {
 
-    }
-    /**
+	}
+	/**
      * Moves object from one point to another over a duration of time
      *
      * @param pointA The object's starting point
@@ -10,58 +10,58 @@ class TarotCard{
      * @param time Time in milliseconds it takes for the object to move from
      * pointA to pointB
      */
-    move(pointA, pointB, time) {
-        //
-    }
+	move(pointA, pointB, time) {
+		//
+	}
 
 
-    /**
+	/**
      * Moves object from one point to another instantly
      *
      * @param pointA The object's starting point
      * @param pointB The object's ending point
      */
-    moveInstantly(pointA, pointB) {
-        //
-    }
+	moveInstantly(pointA, pointB) {
+		//
+	}
 
-    /**
+	/**
      * Rotates object some number of degrees over a duration of time
      *
      * @param degrees Number of degrees to rotate object
-     * @param time Time in milliseconds it takes for the object to complete the 
+     * @param time Time in milliseconds it takes for the object to complete the
      * rotation
      */
-    rotate(degrees, time) {
-        // 
-    }
+	rotate(degrees, time) {
+		//
+	}
 
-    /**
+	/**
      * Rotates object some number of degrees instantly
      *
      * @param degrees Number of degrees to rotate object
      */
-    rotateInstantly(degrees) {
-        //
-    }
+	rotateInstantly(degrees) {
+		//
+	}
 
-    /**
+	/**
      * Flips object over
-     * 
+     *
      * @param faceUp Determines whether the card is face up or not
-     * @param time Time in milliseconds it takes for the object to complete the 
+     * @param time Time in milliseconds it takes for the object to complete the
      * flip
      */
-    setFlip(faceUp, time) {
-        // 
-    }
+	setFlip(faceUp, time) {
+		//
+	}
 
-    /**
+	/**
      * Performs the flip function after a duration of time
      *
      * @param time Time in milliseconds before the flip function is performed
      */
-    setFlipInstantly() {
-        //
-    }
+	setFlipInstantly() {
+		//
+	}
 }
