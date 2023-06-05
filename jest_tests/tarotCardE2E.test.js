620d4a046d22b8b406054cfa6d5159d597509854
daniel-lee-user <danielhlee02@gmail.com>
2023-06-04 20:05:59 -0700

    Added base for tarot card E2E

---
+++ jest_tests/tarotCardE2E.test.js
@@ -0,0 +1,19 @@
+/**
+ * @jest-environment puppeteer
+ */
+describe("Testing Shuffle", () => {    
+    beforeAll(async () => {
+		await page.goto("http://127.0.0.1:5500/index.html");
+	});
+    it("Click on shuffle", async () => {
+		// console.log("Checking for 3 Main Buttons...");
+		// Query select all of the homepage button elements
+		// and return the length of that array
+		const numButtons = await page.$$eval(".home-page", (homePageBtns) => {
+			return homePageBtns.length;
+		});
+		// Expect there that array from earlier to be of length 3
+		// meaning 3 homepage buttons were found
+		expect(numButtons).toBe(3);
+	});
+});
