b8682bad30754bae7fbc4bc24aa56a25734a1eeb
daniel-lee-user <danielhlee02@gmail.com>
2023-06-04 17:28:38 -0700

    Attempt for running puppeteer on GH Actions

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,6 +1,24 @@
-module.exports = {
-    launch: {
-        headless: true,
-        sloMo: 0
-    }
-};
+// get environment varable
+const ci = Boolean(process.env.CI || false);
+
+const baseOptions = {
+  server: {
+    command: 'npm run starting-server',
+    port: 5500
+  }
+}
+
+const ciPipelineOptions = {
+  launch: {
+    headless: true,
+    args: [
+      '--ignore-certificate-errors',
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-accelerated-2d-canvas',
+      '--disable-gpu'
+    ]
+  },
+  server: baseOptions.server
+}
+module.exports = ci ? ciPipelineOptions : baseOptions;
