1ba9fafc0f5c48da42ea1862e0e91c85d804d7ec
daniel-lee-user <daniel-lee-user@users.noreply.github.com>
2023-06-06 00:05:50 +0000

    Apply automatic changes

--- jest_tests/e2e_tests/burgerBarE2E.test.js
+++ jest_tests/e2e_tests/burgerBarE2E.test.js
@@ -9,14 +9,14 @@ const HOMEBTN = "#toHome";
 const GITHUB = "#toGitHub";
 const TAROTCARDBTN = "#toTarotCard";
 const BURGERBAR = "#menu__toggle";
-const BURGERBOX = ".menu__box"
-const BURGERBTNS = ".menu__item"
+const BURGERBOX = ".menu__box";
+const BURGERBTNS = ".menu__item";
 
 
 /**
  * Returns the hidden attribute of the element
  * @param {ElementHandle} element the elementhandle in question
- * @returns {boolean} the hidden value, either true or false
+ * @return {boolean} the hidden value, either true or false
  */
 async function getHiddenVal(element) {
 	const hidden = await element.getProperty("hidden");
@@ -27,7 +27,7 @@ async function getHiddenVal(element) {
 /**
  * Returns the left  attribute of the element
  * @param {String} selector the selector string of the element
- * @returns {number} the left value of the element
+ * @return {number} the left value of the element
  */
 async function getLeftVal(selector) {
 	const leftVal = await page.evaluate((selector) => {
@@ -44,11 +44,11 @@ async function getLeftVal(selector) {
 /**
  * Returns if all elements are hidden or not
  * @param {ElementHandle[]} elements A list of ElementHandles.
- * @returns {boolean} True if all elements are hidden, false otherwise
+ * @return {boolean} True if all elements are hidden, false otherwise
  */
 async function allHidden(elements) {
-	for(let i = 0; i < elements.length; i++) {
-		if((await getHiddenVal(elements[i])) === false) {
+	for (let i = 0; i < elements.length; i++) {
+		if ((await getHiddenVal(elements[i])) === false) {
 			return false;
 		}
 	}
@@ -116,7 +116,7 @@ async function checkHomePageVisible() {
 	const tarotButton = await checkIfVisible("#toTarotCard", page);
 	const eggButton = await checkIfVisible("#eggButton", page);
 	const boneButton = await checkIfVisible("#boneButton", page);
-	return tarotButton && eggButton && boneButton
+	return tarotButton && eggButton && boneButton;
 }
 describe("Testing Burger Bar and Buttons", () => {
 	// visit the fortune telling website
@@ -131,14 +131,14 @@ describe("Testing Burger Bar and Buttons", () => {
 	it("Clicking on Burger Bar Checkbox", async () => {
 		// select the burger bar checkbox
 		const burgerBarCheckbox = await page.$(BURGERBAR);
-		
+
 		// check that the left value of the burgerbox is less than 0
 		expect(await getLeftVal(BURGERBOX)).toBeLessThan(0);
-		
+
 		// make sure check box is not checked
 		const checkedValBefore = await burgerBarCheckbox.getProperty("checked");
 		expect(await checkedValBefore.jsonValue()).toBe(false);
-		
+
 		// click the burger bar checkbox
 		await burgerBarCheckbox.click();
 		await delay(100);
@@ -155,7 +155,7 @@ describe("Testing Burger Bar and Buttons", () => {
 		// Expect there that array from earlier to be of length 6,
 		// meaning 6 burger bar buttons were found
 		expect(bBtns.length).toBe(6);
-		//console.log(await allHidden(bBtns));
+		// console.log(await allHidden(bBtns));
 		// expect btns to not be hidden
 		expect(await allHidden(bBtns)).toBe(false);
 	}, 10000);
@@ -198,7 +198,7 @@ describe("Testing Burger Bar and Buttons", () => {
 		await toTarot.evaluate((b) => b.click()); // click home button
 		await delay(100);
 		expect(await allHidden(homeBtns)).toBe(true);
-		
+
 		await toHome.evaluate((b) => b.click()); // click home button
 		expect(await allHidden(homeBtns)).toBe(false);
 		await delay(100);
