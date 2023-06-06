eb529ffe65d39802ef42f7bd53bbedc7b16179a0
daniel-lee-user <danielhlee02@gmail.com>
2023-06-05 18:15:53 -0700

    did some tab conversion, deleted redundant unit test, made e2e test run twice

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,72 +1,70 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es2022": true,
-        "jest": true,
-        "browser": true,
-    },
-    "extends": [
-        "eslint:recommended",
-        "google"
-    ],
-    "parserOptions": {
-        "parser": "@babel/eslint-parser",
-        requireConfigFile: false,
-        "ecmaVersion": 2022,
-        "sourceType": "module",
-        "allowImportExportEverywhere": true,
-    },
-    "rules": {
-        "max-len": [
-            "error", {
-                "ignoreComments": true
-            }
-        ],
-        "indent": [
-            "error", 
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "camelcase": [
-            "error"
-        ],
-        "no-duplicate-imports": [
-            "error"
-        ],
-        "no-unused-vars":[
-            "warn"
-        ],
-        "brace-style":[
-            "error",
-            "1tbs", { "allowSingleLine": true },
-        ],
-        "no-tabs": 0,
-        "comma-dangle": [
-            "error",
-            "never"
-        ],
-        "eol-last": [
-            "error",
-            "never"
-        ],
-    },
-    "globals": {
-        "Animator": "writeable",
-        "TarotCard": "writeable",
-        "page": "readable"
-    },
-    "ignorePatterns": ['/node_modules/', '/specs/', './github/']
+	"env": {
+		"commonjs": true,
+		"es2022": true,
+		"jest": true,
+		"browser": true,
+	},
+	"extends": [
+		"eslint:recommended",
+		"google"
+	],
+	"parserOptions": {
+		"parser": "@babel/eslint-parser",
+		requireConfigFile: false,
+		"ecmaVersion": 2022,
+		"sourceType": "module",
+		"allowImportExportEverywhere": true,
+	},
+	"rules": {
+		"max-len": [
+			"error", {
+				"ignoreComments": true
+			}
+		],
+		"indent": [
+			"error", 
+			"tab"
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"camelcase": [
+			"error"
+		],
+		"no-duplicate-imports": [
+			"error"
+		],
+		"no-unused-vars":[
+			"warn"
+		],
+		"brace-style":[
+			"error",
+			"1tbs", { "allowSingleLine": true },
+		],
+		"no-tabs": 0,
+		"comma-dangle": [
+			"error",
+			"never"
+		],
+		"eol-last": [
+			"error",
+			"never"
+		],
+	},
+	"globals": {
+		"Animator": "writeable",
+		"TarotCard": "writeable",
+		"page": "readable"
+	},
+	"ignorePatterns": ['/node_modules/', '/specs/', './github/']
 };
-
-
