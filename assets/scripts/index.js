b59c46aabc33b74b4c65165bd72687f7da1f6a7c
ejoa27 <98841828+ejoa27@users.noreply.github.com>
2023-06-08 15:38:17 -0700

    Update index.js
    Deleted some code

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -527,15 +527,3 @@ init();
 document.querySelector(".menuBox").addEventListener("mouseleave", function() {
 	document.querySelector("#menuToggle").checked = false;
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-	const menuToggle = document.querySelector("#menuToggle");
-	if (menuToggle) {
-		menuToggle.addEventListener("change", () => {
-			const menuItems = document.querySelectorAll(".menuItem");
-			menuItems.forEach((item) => {
-				item.hidden = !item.hidden;
-			});
-		});
-	}
-});
